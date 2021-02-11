0154220e91251d855f53c26dee830c3043b079a0
James Li <lijameshao@yahoo.co.uk>
2021-02-11 23:51:32 +0000

    added workflow for rasa forms

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,67 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List, Union, Optional
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+
+
+def create_log(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v)
+    return True
+
+
+class ValidateDailyReflectionForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_daily_reflection_form"
+
+    async def validate_confirm_exercise(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+
+        # (TODO): Decide how and what to validate form
+        if value:
+            # return {"confirm_exercise": True}
+            return {}
+        else:
+            # return {"exercise": "None", "confirm_exercise": False}
+            return {}
+
+
+class ActionSubmitResults(Action):
+    def name(self) -> Text:
+        return "action_submit_results"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+
+        feelings = tracker.get_slot("feelings")
+        stuck_to_plans = tracker.get_slot("stuck_to_plans")
+        focus_on_tasks = tracker.get_slot("focus_on_tasks")
+        strategic = tracker.get_slot("strategic")
+        improvements = tracker.get_slot("improvements")
+        self_reflection = tracker.get_slot("self_reflection")
+
+        response = create_log(
+            feelings=feelings,
+            stuck_to_plans=stuck_to_plans,
+            focus_on_tasks=focus_on_tasks,
+            strategic=strategic,
+            improvements=improvements,
+            self_reflection=self_reflection
+        )
+
+        dispatcher.utter_message("Thanks, your answers have been recorded!")
+        return []
